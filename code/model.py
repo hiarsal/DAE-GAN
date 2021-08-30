7a0176c8ab19ef471a62dc9b88376c2851f841c2
Arsal <shulanruan@163.com>
2021-08-30 09:46:39 +0800

    Update model.py

--- code/model.py
+++ code/model.py
@@ -430,54 +430,6 @@ class INIT_STAGE_G(nn.Module):
         return out_code64
 
 
-class Memory(nn.Module):
-    def __init__(self):
-        super(Memory, self).__init__()
-        self.sm = nn.Softmax()
-        self.mask = None
-
-    def applyMask(self, mask):
-        self.mask = mask  # batch x sourceL
-
-    def forward(self, input, context_key, content_value):#
-        """
-            input: batch x idf x ih x iw (queryL=ihxiw)
-            context: batch x idf x sourceL
-        """
-        ih, iw = input.size(2), input.size(3)
-        queryL = ih * iw
-        batch_size, sourceL = context_key.size(0), context_key.size(2)
-
-        # --> batch x queryL x idf
-        target = input.view(batch_size, -1, queryL)
-        targetT = torch.transpose(target, 1, 2).contiguous()
-        sourceT = context_key
-
-        # Get weight
-        # (batch x queryL x idf)(batch x idf x sourceL)-->batch x queryL x sourceL
-        weight = torch.bmm(targetT, sourceT)
-
-        # --> batch*queryL x sourceL
-        weight = weight.view(batch_size * queryL, sourceL)
-        if self.mask is not None:
-            # batch_size x sourceL --> batch_size*queryL x sourceL
-            mask = self.mask.repeat(queryL, 1)
-            weight.data.masked_fill_(mask.data, -float('inf'))
-        weight = torch.nn.functional.softmax(weight, dim=1)
-
-        # --> batch x queryL x sourceL
-        weight = weight.view(batch_size, queryL, sourceL)
-        # --> batch x sourceL x queryL
-        weight = torch.transpose(weight, 1, 2).contiguous()
-
-        # (batch x idf x sourceL)(batch x sourceL x queryL) --> batch x idf x queryL
-        weightedContext = torch.bmm(content_value, weight)  #
-        weightedContext = weightedContext.view(batch_size, -1, ih, iw)
-        weight = weight.view(batch_size, -1, ih, iw)
-
-        return weightedContext, weight
-
-
 class GET_IMAGE_G(nn.Module):
     def __init__(self, ngf):
         super(GET_IMAGE_G, self).__init__()
